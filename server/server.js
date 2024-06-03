ae3534e669eb204eec4e140740c94d8630a96e49
Sahil <122047068+SahilWadhawan@users.noreply.github.com>
2024-06-03 19:56:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,54 +1,29 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const mongoose = require('mongoose');
-const path = require('path');
-
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-mongoose.connect('mongodb://localhost/chat', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const ChatSchema = new mongoose.Schema({
-  username: String,
-  message: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-const ChatMessage = mongoose.model('ChatMessage', ChatSchema);
-
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  console.log('New user connected');
+    console.log('New user connected');
 
-  ChatMessage.find().then(messages => {
-    socket.emit('init', messages);
-  });
-
-  socket.on('chat', (data) => {
-    const chatMessage = new ChatMessage(data);
-    chatMessage.save().then(() => {
-      io.sockets.emit('chat', data);
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data);
     });
-  });
 
-  socket.on('typing', (data) => {
-    socket.broadcast.emit('typing', data);
-  });
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
+    });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
+
